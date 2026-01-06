38c5a9e1622f16ffa2c38f0e1f7168d92646db95
Bhutoo <bhutoo@gmail.com>
2026-01-06 19:30:33 +0530

    u

--- Functions/script.js
+++ Functions/script.js
@@ -14,3 +14,24 @@ function printTable(n){
 multiplication(3);
 
 printTable(4);
+// functions that return the sum of numbers from 1 to n
+function getSum(n){
+    let sum=0;
+    for(let i=1;i<=n;i++){
+        sum=sum+i;
+    }
+    return sum;
+}
+console.log(getSum(3));
+
+// Concatenation of Strings
+let str = ["hii","hello", "bye", "!"];
+let result = "";
+function concat(str){
+    for(let i=0; i<str.length;i++){
+        result +=str[i];
+    }
+    return result;
+}
+
+console.log(concat(str));
